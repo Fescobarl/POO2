fe5fc55c65a9b35fe143b75530407842b0aa497c
user <user@DESKTOP-T9H70VR>
2023-09-18 14:45:47 -0500

    carga inicial

---
+++ src/main/java/UMLfor/PruebaFigura.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UMLfor;
+
+
+public class PruebaFigura {
+    public static void main(String args[]) {
+        formulariofiguras form = new formulariofiguras();
+        form.setVisible(true);
+    }
+}
