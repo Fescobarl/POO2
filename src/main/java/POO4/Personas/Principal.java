f7024536e9ecac545eef5e41a3921644afe28200
user <user@DESKTOP-T9H70VR>
2023-10-12 15:21:10 -0500

    carga inicial

---
+++ src/main/java/POO4/Personas/Principal.java
@@ -0,0 +1,11 @@
+package POO4.Personas;
+
+public class Principal {
+ 
+public static void main(String[] args) {
+VentanaPrincipal miVentanaPrincipal; 
+miVentanaPrincipal= new VentanaPrincipal(); 
+miVentanaPrincipal.setVisible(true); 
+}
+}
+
