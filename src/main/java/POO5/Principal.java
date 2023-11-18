0afab6aadf76fdc7c09a18de5ca25473278fd610
user <user@DESKTOP-T9H70VR>
2023-11-17 22:18:51 -0500

    carga inicial

---
+++ src/main/java/POO5/Principal.java
@@ -0,0 +1,11 @@
+
+package POO5;
+
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
